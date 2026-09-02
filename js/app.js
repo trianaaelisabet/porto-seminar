fdc80223e648471aba8c8d41a0529174c35b71f6
trianaaelisabet <trianaelisabet.2025@student.uny.ac.id>
2026-09-02 13:36:22 +0700

    delete button read full app.js

--- js/app.js
+++ js/app.js
@@ -140,10 +140,6 @@ function renderSemesterTimeline(containerId, courses) {
         </div>
 
         <div class="reflection-actions">
-          <button class="btn btn-secondary btn-read-reflection" data-course-id="${course.id}">
-            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path></svg>
-            Read Full Reflection
-          </button>
           <button class="btn btn-gold btn-view-artifact" data-course-id="${course.id}">
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line></svg>
             View Artifact
